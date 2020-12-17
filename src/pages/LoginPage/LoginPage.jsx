968343973ba8abc404ca9e0523458e4460e1f0cf
Micke Berg <micke.berg.dev@gmail.com>
2020-12-17 23:17:06 +0100

    Login Page

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './LoginPage.scss';
+import SignIn from '../../components/SignIn/SignIn';
+
+function LoginPage() {
+  return (
+    <SignIn />
+  );
+}
+
+export default LoginPage;
