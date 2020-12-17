69a2e88911014b1f364e6f43fe4ffd3c368b9345
Micke Berg <micke.berg.dev@gmail.com>
2020-12-17 16:49:55 +0100

    Add Main and Home page layout and routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,35 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './default.scss';
 
+// Layouts
+import MainLayout from './layouts/MainLayout';
+import HomepageLayout from './layouts/HomePageLayout';
+
 import HomePage from './pages/Homepage/HomePage';
-import Header from './components/Header/Header';
 import Registration from './pages/Registration/Registration';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/registration" component={Registration} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <HomepageLayout>
+              <HomePage />
+            </HomepageLayout>
+          )}
+        />
+        <Route
+          path="/registration"
+          render={() => (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
