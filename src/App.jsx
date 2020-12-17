ddd8dff6330db933f939f8171d3270dc0c4c8036
Micke Berg <micke.berg.dev@gmail.com>
2020-12-17 14:25:17 +0100

    Add registration page and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './default.scss';
 
 import HomePage from './pages/Homepage/HomePage';
 import Header from './components/Header/Header';
+import Registration from './pages/Registration/Registration';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="main">
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/registration" component={Registration} />
+        </Switch>
       </div>
     </div>
   );
