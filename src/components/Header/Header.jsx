27ed4ee32b5cb7510d16fbd855061279396aba53
Micke Berg <micke.berg.dev@gmail.com>
2020-12-18 19:07:26 +0100

    Add conditionals log in and out.  onClick signOut

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import './styles.scss';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/utils';
 
-function Header() {
+function Header(props) {
+  const { currentUser } = props;
+  console.log(currentUser);
   return (
     <header className="header">
       <div className="wrapper">
@@ -12,14 +17,23 @@ function Header() {
           </Link>
         </div>
         <div className="callToActions">
-          <ul>
-            <li>
-              <Link to="/registration">Register</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
+          {currentUser && (
+            <ul>
+              <li>
+                <span onClick={() => auth.signOut()}>Log out</span>
+              </li>
+            </ul>
+          )}
+          {!currentUser && (
+            <ul>
+              <li>
+                <Link to="/registration">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </header>
