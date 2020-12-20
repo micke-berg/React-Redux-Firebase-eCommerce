2c835ea6f3dd7256bfd4e15c2b6716b4d0f2abb3
Micke Berg <micke.berg.dev@gmail.com>
2020-12-20 10:58:19 +0100

    Add sing in with email and password

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignIn.scss';
 
 import Button from '../forms/Button/Button';
-import { signInWithGoogle } from '../../firebase/utils';
+import FormInput from '../forms/FormInput/FormInput';
+
+import { signInWithGoogle, auth } from '../../firebase/utils';
 
 function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      setEmail('');
+      setPassword('');
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -15,6 +34,21 @@ function SignIn() {
         <h2>Log in</h2>
         <div className="form-wrap">
           <form onSubmit={handleSubmit}>
+            <FormInput
+              type="email"
+              name="email"
+              value={email}
+              placeholder="Email address..."
+              onChange={handleEmailChange}
+            />
+            <FormInput
+              type="password"
+              name="password"
+              value={password}
+              placeholder="password..."
+              onChange={handlePasswordChange}
+            />
+            <Button type="submit">Login</Button>
             <div className="social-signIn">
               <div className="row">
                 <Button onClick={signInWithGoogle}>Sign In With Google</Button>
