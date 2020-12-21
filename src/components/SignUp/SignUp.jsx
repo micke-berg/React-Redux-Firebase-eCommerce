14d6b99a1586cdbe6eed74d6d7dc9106dc0870b5
Micke Berg <micke.berg.dev@gmail.com>
2020-12-21 14:01:30 +0100

    Add handleChange and resetForm

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
 import './SignUp.scss';
 
 import { auth, handleUserProfile } from '../../firebase/utils';
@@ -7,24 +8,19 @@ import FormInput from '../forms/FormInput/FormInput';
 import AuthWrapper from '../AuthWrapper/AuthWrapper';
 import Button from '../forms/Button/Button';
 
-function SignUp() {
+function SignUp(props) {
   const [displayName, setDisplayName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [errors, setErrors] = useState('');
 
-  const handleNameChange = (e) => {
-    setDisplayName(e.target.value);
-  };
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-  const handleConfirmPasswordChange = (e) => {
-    setConfirmPassword(e.target.value);
+  const resetFormStates = () => {
+    setDisplayName('');
+    setEmail('');
+    setPassword('');
+    setConfirmPassword('');
+    setErrors([]);
   };
 
   const handleFormSubmit = async (e) => {
@@ -32,16 +28,12 @@ function SignUp() {
     if (password !== confirmPassword) {
       const err = ['Passwords don´t match'];
       setErrors(err);
-      console.log(err);
     }
     try {
       const { user } = await auth.createUserWithEmailAndPassword(email, password);
       await handleUserProfile(user, { displayName });
-
-      setDisplayName('');
-      setEmail('');
-      setPassword('');
-      setConfirmPassword('');
+      resetFormStates();
+      props.history.push('/');
     } catch (err) {
       setErrors(err);
     }
@@ -66,36 +58,35 @@ function SignUp() {
             type="text"
             name="displayName"
             value={displayName}
-            placeholder="Full Name..."
-            onChange={handleNameChange}
+            placeholder="Full name"
+            handleChange={(e) => setDisplayName(e.target.value)}
           />
           <FormInput
             type="email"
             name="email"
             value={email}
-            placeholder="Email..."
-            onChange={handleEmailChange}
+            placeholder="Email"
+            handleChange={(e) => setEmail(e.target.value)}
           />
           <FormInput
             type="password"
             name="password"
             value={password}
-            placeholder="password..."
-            onChange={handlePasswordChange}
+            placeholder="Password"
+            handleChange={(e) => setPassword(e.target.value)}
           />
           <FormInput
             type="password"
             name="confirmPassword"
             value={confirmPassword}
-            placeholder="Confirm Password..."
-            onChange={handleConfirmPasswordChange}
+            placeholder="Confirm Password"
+            handleChange={(e) => setConfirmPassword(e.target.value)}
           />
           <Button type="submit">Register</Button>
         </form>
       </div>
     </AuthWrapper>
-
   );
 }
 
-export default SignUp;
+export default withRouter(SignUp);
