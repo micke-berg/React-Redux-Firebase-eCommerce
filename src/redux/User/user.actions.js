f472ab52e9564f8b56d4a77071d7df192d7e5e9f
Micke Berg <micke.berg.dev@gmail.com>
2020-12-21 15:45:05 +0100

    SignInUser action

--- src/redux/User/user.actions.js
+++ src/redux/User/user.actions.js
@@ -1,8 +1,21 @@
 import userTypes from './user.types';
+import { auth } from '../../firebase/utils';
 
 const setCurrentUser = (user) => ({
   type: userTypes.SET_CURRENT_USER,
   payload: user,
 });
+
+export const signInUser = ({ email, password }) => async (dispatch) => {
+  try {
+    await auth.signInWithEmailAndPassword(email, password);
+    dispatch({
+      type: userTypes.SIGN_IN_SUCCESS,
+      payload: true,
+    });
+  } catch (err) {
+    // console.log(err);
+  }
+};
 
 export default setCurrentUser;
