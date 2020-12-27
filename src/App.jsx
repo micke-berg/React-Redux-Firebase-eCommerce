901bdd24e406171feb48251251b138ece4e5400b
Micke Berg <micke.berg.dev@gmail.com>
2020-12-27 13:50:05 +0100

    Add search filterType route

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,16 @@ const App = () => {
           )}
         />
         <Route
+          exact
           path="/search"
+          render={() => (
+            <MainLayout>
+              <Search />
+            </MainLayout>
+          )}
+        />
+        <Route
+          path="/search/:filterType"
           render={() => (
             <MainLayout>
               <Search />
